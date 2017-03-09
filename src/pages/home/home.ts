25f88d5c9804defd2281ed8dab0bf6ec5dfdebb1
franciscoprado <franciscontato@gmail.com>
2017-03-09 14:23:23 -0300

    adicionado preloader inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Events, NavController } from 'ionic-angular';
+import { Events, NavController, LoadingController } from 'ionic-angular';
 import { WeatherService } from '../../services/weather';
 import { Geolocation } from 'ionic-native';
 
@@ -10,14 +10,36 @@ import { Geolocation } from 'ionic-native';
 
 export class HomePage {
   private tempo: any;
+  private loader: any;
 
-  constructor(public navCtrl: NavController, public localWeather: WeatherService, public events: Events) {
+  constructor(public navCtrl: NavController, public localWeather: WeatherService, public events: Events, public loadingCtrl: LoadingController) {
     this.tempo = localWeather;
 
+    this.addChangeCityEventListener(events);
+    this.addPreloaderEventListener(events);
+    this.addGeolocationSupport();
+  }
+
+  addChangeCityEventListener(events: Events) {
     events.subscribe('changeCity', (city) => {
       console.log('mudou para', city);
     });
+  }
 
+  addPreloaderEventListener(events: Events) {
+    this.loader = this.loadingCtrl.create({
+      content: "Aguarde, carregando previsão..."
+    });
+    this.loader.present();
+
+    events.subscribe('forecastLoaded', (status) => {
+      if (status == '200') {
+
+      }
+    });
+  }
+
+  addGeolocationSupport() {
     Geolocation.getCurrentPosition().then((resp) => {
       // resp.coords.latitude
       // resp.coords.longitude
@@ -35,10 +57,6 @@ export class HomePage {
       console.log("COORDENADAS: ", data.coords);
       this.tempo.obterPrevisaoPorCoordenadas(data.coords.latitude, data.coords.longitude);
     });
-  }
-
-  public updateHome(city: string) {
-    console.log('atualizar para cidade:', city);
   }
 
 }
