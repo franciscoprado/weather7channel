8b8d27852e0c9ffc5cc9b892f8f0c8f36d5e25e5
franciscoprado <franciscontato@gmail.com>
2017-03-10 13:42:49 -0300

    bugfix - exibição da data e formatação

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -3,19 +3,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'formatDate'})
 export class FormatDatePipe implements PipeTransform {
     transform(value: Date, arg): any {
-        let day: number = value.getDay();
+        let date: number = value.getDate();
         let month: number = value.getMonth() + 1;
-        let fday: string = value.getDay().toString();
+        let fdate: string = value.getDate().toString();
         let fmonth: string = value.getMonth().toString();
 
-        if (day < 10) {
-            fday = '0' + day.toString();
+        console.log(value);
+
+        if (date < 10) {
+            fdate = '0' + date.toString();
         }
 
         if (month < 10) {
             fmonth = '0' + month.toString();
         }
 
-        return fday + '/' + fmonth;
+        return fdate + '/' + fmonth;
     }
 }
